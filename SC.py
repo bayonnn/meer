c7f66165532a9db037ab316cd14c08a655143dfb
bayonnn <105988400+bayonnn@users.noreply.github.com>
2022-05-22 01:00:14 +0700

    Add files via upload

---
+++ SC.py
@@ -0,0 +1,17 @@
+import random
+import socket
+import threading
+import time
+import os,sys
+
+os.system('clear')
+print("\033[1;31;40m")
+print("""
+
+╭━━━━╮╱╱╱╱╱╱╱╭━╮╭━╮
+╰━━╮━┃╱╱╱╱╱╱╱╰╮╰╯╭╯
+╱╱╭╯╭╋━━┳━┳━━╮╰╮╭╯
+╱╭╯╭╯┃┃━┫╭┫╭╮┃╭╯╰╮
+╭╯━╰━┫┃━┫┃┃╰╯┣╯╭╮╰╮
+╰━━━━┻━━┻╯╰━━┻━╯╰━╯""")
+print("MAKAN NIH SC KNTOL,GATAU SUSAH APA BUATNYA")
